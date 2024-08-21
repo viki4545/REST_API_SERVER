c400c884f2f3c7a3fd5020599471d5d8633be2f9
Vicky <vickygupta9987@gmail.com>
2024-08-21 21:48:53 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
 const crudRouter = require("./routes/crudRoutes");
 const cors = require("cors");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
